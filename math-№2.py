1b99542ea7bed198cd272cd09c05249654c04a22
DataFIex <77386386+DataFIex@users.noreply.github.com>
2021-01-17 01:54:20 +0300

    Update math-№2.py

--- math-№2.py
+++ math-№2.py
@@ -1,9 +1,9 @@
 min=10000000000
 for x in range(-1000, 1001):
     x=x/1000
-    for y in range(-100, 101):
-        y=y/100
-        a=6*x**2 + 2*(x**4)*y**2 + 10*(x**2 + y**2) + 4 + 6*x*(y-1)
+    for y in range(-1000, 1001):
+        y=y/1000
+        a=6*x**6 + 2*(x**4)*y**2 + 10*(x**2 + y**2) + 4 + 6*x*(y-1)
         if a<min:
             min=a
 print(min)
